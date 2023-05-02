dae4562bfd482a6f36eb3ff25546d64f0c6bcf94
Takumi Araki <apndx19@gmail.com>
2023-05-03 01:16:59 +0900

    modifying rEADME.md

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # DEBUG
 
+# Usage
+```C
+//include debug.h to use utilities
 #include  "debug.h"
+```
 
-
+```C
 //use STOP as follows
 while (1)
 {
-  i++;
-  STOP;
-  printf("%d\n", i);
+	i++;
+	STOP; //wait for the user input
+	printf("%d\n", i);
 }
+```
+```C
+//use TESTn(i) as follows
+while (1)
+{
+	i++;
+	TESTn(i); //displays 1.FILE NAME, 2.LINE, 3.FUNCTION NAME, and 4.VALUE
+}
+```
--- destructor.c
+++ destructor.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 20:36:04 by taaraki           #+#    #+#             */
-/*   Updated: 2023/05/01 21:02:25 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/05/03 01:09:32 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include	<stdlib.h>
@@ -14,6 +14,6 @@
 __attribute__((destructor))
 void	destructor(void)
 {
-	//system("leaks -q push_swap");
+	system("echo");
 	system("leaks -q push_swap | awk 'NR == 3' | cut -d ' ' -f 3-4");
 }
